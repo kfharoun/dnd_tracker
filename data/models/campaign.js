7ca2933afd546f10841315379efe857b876fa0e3
kfharoun <kassandra.ferland@gmail.com>
2024-06-21 14:09:21 -0400

    campaign update

--- data/models/campaign.js
+++ data/models/campaign.js
@@ -3,7 +3,9 @@ const { Schema } = require('mongoose')
 const CampaignSchema = new Schema(
   {
     campaign_name: { type: String, required: true },
-    campaign_info: { type: String, required: false }
+    campaign_info: { type: String, required: false }, 
+    dungeon_master: {type: String, required: true}, 
+    players: [{type: String, required: true}]
   },
   { timestamps: true }
 )
