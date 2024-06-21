9a6803072a0dfd5de6d5d2e30a821906077eea2e
kfharoun <kassandra.ferland@gmail.com>
2024-06-21 14:40:35 -0400

    Update campaign and character models and seed

--- data/models/character.js
+++ data/models/character.js
@@ -3,10 +3,13 @@ const { Schema } = require('mongoose')
 const CharacterSchema = new Schema(
   {
     character_name: { type: String, required: true },
+    played_by: {type: String, required: true}, 
     character_image: {type: String, required: false},
     race: { type: String, required: true },
     class_name: {type: String, required: true},
     subclass_name: { type: String, required: true },
+    armor_class: {type: Number, required: true}, 
+    hit_points: {type: Number, required: true}, 
     strength: { type: Number, required: true },
     dexterity: { type: Number, required: true },
     constitution: { type: Number, required: true },
