02749a8b44079e26572ea4ce761940512fe0ddca
kfharoun <kassandra.ferland@gmail.com>
2024-06-22 15:04:12 -0400

    Update home styling and font color

--- dndProject/src/components/Home.jsx
+++ dndProject/src/components/Home.jsx
@@ -5,9 +5,13 @@ export default function Home () {
         <div className="Home">
             <div class="homepic"></div>
             <div className='welcomehome'>
+                
                 <h1 className='welcome'>Welcome Traveler</h1>
-                <button className='linkbutton'><Link to ="/Campaign" className='buttonlink'>record your adventures</Link></button>
-                <Link to="/Character" className='buttonlink'><button className='linkbutton'>new party members?</button></Link>
+
+                <button className='linkbutton one'><Link to ="/Campaign" className='buttonlink'>record your adventures</Link></button>
+
+                <Link to="/Character" className='buttonlink'><button className='linkbutton two'>new party members?</button></Link>
+
             </div>
         </div>
     )
